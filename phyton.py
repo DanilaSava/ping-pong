a435c47cb98fdc837bce256741a656774410bfbf
zelenka <166910859+DanilaSava@users.noreply.github.com>
2024-05-11 13:42:19 +0300

    Update phyton.py

--- phyton.py
+++ phyton.py
@@ -12,8 +12,13 @@ clock = time.Clock()
 FPS = 120
 
 
+speed_x = 3
+speed_y = 3
 
 
+
+#k = mixer.Sound("ping-pong.mp3")
+#q = mixer.Sound("ping-pong2.mp3")
 
 
 background = transform.scale(image.load("field.png"), (700, 393))
@@ -42,8 +47,10 @@ class Player(GameSprite):
         if keys_pressed [K_w] and self.rect.y > 0:
             self.rect.y -= self.speed
 
-        if keys_pressed [K_s] and self.rect.y < 250:
+        if keys_pressed [K_s] and self.rect.y < 350:
             self.rect.y += self.speed
+
+  
 
     def update1(self):
         
@@ -52,9 +59,55 @@ class Player(GameSprite):
         if keys_pressed [K_UP] and self.rect.y > 0:
             self.rect.y -= self.speed
 
-        if keys_pressed [K_DOWN] and self.rect.y < 250:
+        if keys_pressed [K_DOWN] and self.rect.y < 350:
             self.rect.y += self.speed
 
+class Ball(GameSprite):
+    def __init__(self, player_image, width, height, speed, player_x, player_y, speed_x, speed_y):
+        super().__init__(player_image, width, height, speed, player_x, player_y)
+        self.speed_x = speed_x
+        self.speed_y = speed_y
+
+    def update(self):
+        ball.rect.y += self.speed_y
+        ball.rect.x += self.speed_x
+ 
+
+    def hit(self, paddle):
+        if sprite.collide_rect(self, paddle):
+            #k.play()
+            angle = section(paddle, self)
+            if abs(angle) == 40:
+                self.speed_x = 3
+                self.speed_y = 3
+            elif abs(angle) == 20:
+                self.speed_x = 3
+                self.speed_y = 1
+            elif abs(angle) == 0:
+                self.speed_x = 3
+                self.speed_y = 0
+            if angle < 0:
+                self.speed_y *= -1
+            if self.rect.x >= 450:
+                self.speed_x *= -1
+
+
+
+
+
+
+def section (paddle, pong):
+    height = paddle.rect.bottom - paddle.rect.top 
+    if pong.rect.centery <= (paddle.rect.y + height * 1/4):
+        return -40
+    elif pong.rect.centery <= (paddle.rect.y + height * 2/5):
+        return -20
+    elif pong.rect.centery <= (paddle.rect.y + height * 3/5):
+        return 0
+    elif pong.rect.centery <= (paddle.rect.y + height * 4/5):
+        return 20
+    elif pong.rect.centery <= (paddle.rect.y + height * 5/5):
+        return 40
 
 
 
@@ -63,10 +116,11 @@ class Player(GameSprite):
 
 
 
-ball = GameSprite("ball.png", 50, 50, 7, 340, 163)
 
-player1 = Player("player1.png", 40, 150, 2, 0, 163)
-player2 = Player("player2.png", 55, 150, 2, 660, 163)
+ball = Ball("ball.png", 50, 50, 7, 340, 163, 2, 2)
+
+player1 = Player("player1.png", 40, 150, 1, 0, 163)
+player2 = Player("player2.png", 55, 150, 1, 660, 163)
 
 
 game = True
@@ -75,8 +129,7 @@ finish = False
 
 
 
-speed_x = 2
-speed_y = 2
+
 
 
 
@@ -100,8 +153,7 @@ while game:
     if finish != True:
         
 
-        ball.rect.y += speed_y
-        ball.rect.x += speed_x
+
 
         window.blit(background,(0, 0))
     
@@ -112,52 +164,54 @@ while game:
         window.blit(schet1,(10, 10))
         window.blit(schet2,(500, 10))
 
+
+
+
+        if ball.rect.y > 343 or ball.rect.y < 0:
+            ball.speed_y *= -1
+
+
+
+
+        if schetP1 > 10 and schetP1 - schetP2 >= 2:
+            window.blit(win1, (230, 180))
+            finish = True
+
+        if schetP2 > 10 and schetP2 - schetP1 >= 2:
+            window.blit(win2, (230, 180))
+            finish = True
+
+
+        if ball.rect.x < -50:
+            schetP2 += 1
+            ball.kill()
+            ball = Ball("ball.png", 50, 50, 7, 340, 163, 1, 1)
+
+        
+        if ball.rect.x > 800:
+            schetP1 += 1
+            ball.kill()
+            ball = Ball("ball.png", 50, 50, 7, 340, 163, 1, 1)
+
+        
+
+        ball.hit(player1)
+        ball.hit(player2)
+        ball.update()
+        ball.reset() 
         player1.update()
         player1.reset()
         player2.update1()
         player2.reset()
-        ball.update()
-        ball.reset()
-
-
-    if ball.rect.y > 343 or ball.rect.y < 0:
-        speed_y *= -1
-
-
-
-    if sprite.collide_rect(player1, ball) or sprite.collide_rect(player2, ball):
-        speed_x *= -1
-      
-
-
-
-    if ball.rect.x < -50:
-        schetP2 += 1
-        ball.kill()
-        ball = GameSprite("ball.png", 50, 50, 7, 340, 163)
-
-    
-    if ball.rect.x > 800:
-        schetP1 += 1
-        ball.kill()
-        ball = GameSprite("ball.png", 50, 50, 7, 340, 163)
-
-
-    if schetP1 > 2:
-        window.blit(win1, (230, 180))
-        finish = True
-
-    if schetP2 > 2:
-        window.blit(win2, (230, 180))
-        finish = True
-
-
 
 
 
 
     clock.tick(FPS)
     display.update()
+
+    
+
 
     
 
